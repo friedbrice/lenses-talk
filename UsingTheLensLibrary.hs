b870df80a5ee00207638859fa2ff4f1e32e15e1d
Daniel P. Brice <danielbrice@gmail.com>
2019-02-20 15:00:40 -0800

    uncomment partial field traversals

--- UsingTheLensLibrary.hs
+++ UsingTheLensLibrary.hs
@@ -36,20 +36,20 @@ data Payment
             }
     deriving (Generic, Show)
 
--- class HasPartialField (symbol :: Symbol) s a | symbol s -> a where
---     fieldP :: Traversal' s a
+class HasPartialField (symbol :: Symbol) s a | symbol s -> a where
+    fieldP :: Traversal' s a
 
--- instance HasPartialField "cardNumber" Payment Int where
---     fieldP f (Credit cst amt cn) = fmap (\x -> Credit cst amt x) (f cn)
---     fieldP _ pmt = pure pmt
+instance HasPartialField "cardNumber" Payment Int where
+    fieldP f (Credit cst amt cn) = fmap (\x -> Credit cst amt x) (f cn)
+    fieldP _ pmt = pure pmt
 
--- instance HasPartialField "routingNumber" Payment Int where
---     fieldP f (Check c a r n) = fmap (\x -> Check c a x n) (f r)
---     fieldP _ pmt = pure pmt
+instance HasPartialField "routingNumber" Payment Int where
+    fieldP f (Check c a r n) = fmap (\x -> Check c a x n) (f r)
+    fieldP _ pmt = pure pmt
 
--- instance HasPartialField "accountNumber" Payment Int where
---     fieldP f (Check c a r n) = fmap (\x -> Check c a r x) (f n)
---     fieldP _ pmt = pure pmt
+instance HasPartialField "accountNumber" Payment Int where
+    fieldP f (Check c a r n) = fmap (\x -> Check c a r x) (f n)
+    fieldP _ pmt = pure pmt
 
 daniel :: Customer
 daniel = Customer "Daniel" 34
